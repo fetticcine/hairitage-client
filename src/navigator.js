58e076908a0e9042351b20e7aff35870b26616a9
Abdul Ali <aliabdul5@gmail.com>
2018-07-14 14:27:34 -0700

    add header to each tab screen

--- src/navigator.js
+++ src/navigator.js
@@ -1,23 +1,46 @@
-import Home from './screens/Home';
-import Discover from './screens/Discover';
-import Inbox from './screens/Inbox';
-import Profile from './screens/Profile';
+import HomeScreen from './screens/Home';
+import DiscoverScreen from './screens/Discover';
+import InboxScreen from './screens/Inbox';
+import ProfileScreen from './screens/Profile';
 
-import { createBottomTabNavigator } from 'react-navigation';
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from 'react-navigation';
+
+const Home = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: { title: 'Home' }
+  }
+});
+
+const Discover = createStackNavigator({
+  Discover: {
+    screen: DiscoverScreen,
+    navigationOptions: { title: 'Discover' }
+  }
+});
+
+const Inbox = createStackNavigator({
+  Inbox: {
+    screen: InboxScreen,
+    navigationOptions: { title: 'Inbox' }
+  }
+});
+
+const Profile = createStackNavigator({
+  Profile: {
+    screen: ProfileScreen,
+    navigationOptions: { title: 'Profile' }
+  }
+});
 
 const AppNavigator = createBottomTabNavigator({
-  Home: {
-    screen: Home
-  },
-  Discover: {
-    screen: Discover
-  },
-  Inbox: {
-    screen: Inbox
-  },
-  Profile: {
-    screen: Profile
-  }
+  Home,
+  Discover,
+  Inbox,
+  Profile
 });
 
 export default AppNavigator;
