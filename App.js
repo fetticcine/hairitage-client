440b74a021a655d5686bf742498b0b07ecc0e762
Abdul Ali <aliabdul5@gmail.com>
2018-07-14 13:18:36 -0700

    set up basic navigation + highest level screens

--- App.js
+++ App.js
@@ -1,23 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import AppNavigator from './src/navigator';
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <AppNavigator />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
