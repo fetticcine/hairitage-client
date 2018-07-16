95830ebf23a5c016581a2dbc0ef5e2222ca0ba11
Fadhi Ali <fadhi.ahmed.ali@gmail.com>
2018-07-15 20:35:17 -0700

    add messages list and container

---
+++ src/components/messageListContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import data from '../data/messageData.json';
+import MessagesList from './MessagesList';
+
+class messageListContainer extends React.Component {
+    state = {
+        data: []
+    }
+
+    componentDidMount() {
+        this.setState({
+            data
+        })
+    }
+
+    render() {
+        return (
+            <MessagesList data={this.state.data} />
+        );
+    }
+}
+
+export default messageListContainer;
