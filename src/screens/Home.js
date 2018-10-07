136b22abb8e034a82d194dcde3d35b8d0a2ce4fb
Fadhi Ali <fadhi.ahmed.ali@gmail.com>
2018-10-07 13:04:36 -0500

    render socialPostList on home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
 import SocialPostList from '../components/post/SocialPostList';
-import SocialPost from '../components/post/SocialPost';
 
 export default class Home extends React.Component {
   render() {
     return (
-      <SocialPost /> 
+      <SocialPostList />
     );
   }
 }
