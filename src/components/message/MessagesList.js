b52bad4a447fc88c15673fc72c6cc28a154901c1
Abdul Ali <aliabdul5@gmail.com>
2018-10-08 22:38:09 -0500

    wip

--- src/components/message/MessagesList.js
+++ src/components/message/MessagesList.js
@@ -6,36 +6,40 @@ import MessageRow from '../message/MessageRow';
 
 // TODO: make styles dynamic
 const imageStyle = {
-    height: 40,
-    width: 40,
-    borderRadius: 20,
-    marginRight: 10
+  height: 40,
+  width: 40,
+  borderRadius: 20,
+  marginRight: 10
 };
 
 // TODO: look at socialPost to mirror passing image urls as props and use gdrive for image hosting
 class MessagesList extends React.Component {
-    renderItem = ({item}) => {
-        return (
-        <MessageRow
-            senderName={item.senderName}
-            msgPreviewText={item.msgPreviewText}
-            image={<Image style={imageStyle} source={require('../../assets/fade.jpg')} />}
-            timeStamp={item.timeStamp} 
-        />
-    )};
+  renderItem = ({ item }) => {
+    return (
+      <MessageRow
+        senderName={item.senderName}
+        msgPreviewText={item.msgPreviewText}
+        image={
+          <Image style={imageStyle} source={require('../../assets/fade.jpg')} />
+        }
+        timeStamp={item.timeStamp}
+      />
+    );
+  };
 
-    render() {
-        return (
-            <FlatList
-                data={this.props.data}
-                renderItem={this.renderItem} 
-            />
-        );
-    }
+  render() {
+    return (
+      <FlatList
+        data={this.props.data}
+        renderItem={this.renderItem}
+        keyExtractor={(_, index) => index.toString()}
+      />
+    );
+  }
 }
 
 MessagesList.propTypes = {
-    data: PropTypes.array.isRequired,
+  data: PropTypes.array.isRequired
 };
 
 export default MessagesList;
--- src/screens/Inbox.js
+++ src/screens/Inbox.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import MessageListContainer from '../components/message/messageListContainer';
 
 export default class Inbox extends React.Component {
+  state = {
+    onMessageView: true
+  };
+
+  onMessagesPress = () => {
+    if (!this.state.onMessageView) this.setState({ onMessageView: true });
+  };
+
+  onNotificationsPress = () => {
+    if (this.state.onMessageView) this.setState({ onMessageView: false });
+  };
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Inbox Screen</Text>
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-around'
+          }}
+        >
+          <TouchableOpacity onPress={this.onMessagesPress}>
+            <Text>Messages</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.onNotificationsPress}>
+            <Text>Notifications</Text>
+          </TouchableOpacity>
+        </View>
+        {this.state.onMessageView ? (
+          <MessageListContainer />
+        ) : (
+          <Text>Notifications screen</Text>
+        )}
       </View>
     );
   }
