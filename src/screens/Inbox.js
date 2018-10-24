6b290d0a2e9f01520ca0730d6051ac04438345f9
Fadhi Ali <fadhi.ahmed.ali@gmail.com>
2018-10-23 20:32:50 -0500

    added new message button, cleaned up styling

--- src/screens/Inbox.js
+++ src/screens/Inbox.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 import MessageListContainer from '../components/message/messageListContainer';
+import BaseButton from '../components/common/BaseButton';
 
 export default class Inbox extends React.Component {
   state = {
@@ -17,28 +18,54 @@ export default class Inbox extends React.Component {
   };
 
   render() {
+    const { tabsContainer, tabText, selectedButton, unselectedButton } = styles;
     return (
-      <View style={{ flex: 1 }}>
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-around'
-          }}
-        >
-          <TouchableOpacity onPress={this.onMessagesPress}>
-            <Text>Messages</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.onNotificationsPress}>
-            <Text>Notifications</Text>
-          </TouchableOpacity>
+      <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        <View style={tabsContainer}>
+          <View style={this.state.onMessageView ? selectedButton : unselectedButton}>
+            <TouchableOpacity onPress={this.onMessagesPress}>
+              <Text style={tabText}>MESSAGES</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={this.state.onMessageView ? unselectedButton : selectedButton}>
+            <TouchableOpacity onPress={this.onNotificationsPress}>
+              <Text style={tabText}>NOTIFICATIONS</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        {this.state.onMessageView ? (
-          <MessageListContainer />
-        ) : (
-          <Text>Notifications screen</Text>
-        )}
+        <BaseButton 
+          text='NEW MESSAGE'
+          pdgHorizontal={70}
+          backgroundColor='#c4c4c4'
+          textColor='#fff'
+        />
+        {this.state.onMessageView ? (<MessageListContainer />) : (<Text>Notifications screen</Text>)}
       </View>
     );
   }
 }
+
+const styles = {
+  tabsContainer: {
+      flexDirection: 'row', 
+      justifyContent: 'space-around',
+      marginTop: 20,
+      marginBottom: 10,
+      marginLeft: 50,
+      marginRight: 50,
+  },
+  tabText: {
+      fontFamily: 'Futura-CondensedMedium',
+      fontSize: 18,
+      color: '#c4c4c4',
+      paddingBottom: 2,
+  },
+  selectedButton: {
+      borderColor: '#c4c4c4',
+      borderBottomWidth: 2,
+  },
+  unselectedButton: {
+      backgroundColor: 'transparent',
+  },
+};
+
