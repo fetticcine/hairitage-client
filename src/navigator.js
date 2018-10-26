629ec43e6407f8b6dad49f43a969e7cb1ee06348
Fadhi Ali <fadhi.ahmed.ali@gmail.com>
2018-10-25 19:14:44 -0500

    added settings screen to profile stack navigator; added settings gear icon

--- src/navigator.js
+++ src/navigator.js
@@ -2,6 +2,11 @@ import HomeScreen from './screens/Home';
 import DiscoverScreen from './screens/Discover';
 import InboxScreen from './screens/Inbox';
 import ProfileScreen from './screens/Profile';
+import SettingsScreen from './screens/Settings';
+
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import {
   createBottomTabNavigator,
@@ -56,15 +61,37 @@ const Inbox = createStackNavigator({
 const Profile = createStackNavigator({
   Profile: {
     screen: ProfileScreen,
-    navigationOptions: { 
+    navigationOptions: ({navigation}) => ({ 
       title: 'PROFILE',
+      headerRight: 
+      <TouchableOpacity 
+        onPress={() => navigation.navigate('Settings')}
+        style={{ position: 'absolute', right: 0, marginRight: 5, }}>
+          <Icon name="settings" size={20} color="#c4c4c4" />
+      </TouchableOpacity>,
       headerTintColor: '#C4C4C4',
       headerTitleStyle: {
         fontFamily: 'Futura-CondensedMedium',
         fontSize: 16,
         fontWeight: '600',
       },
-    }
+    })
+  },
+  Settings: {
+    screen: SettingsScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'SETTINGS',
+      headerLeft:
+      <TouchableOpacity onPress={() => navigation.goBack()} >
+          <Icon name="chevron-left" size={30} color="#c4c4c4" />
+      </TouchableOpacity>,
+      headerTintColor: '#C4C4C4',
+      headerTitleStyle: {
+        fontFamily: 'Futura-CondensedMedium',
+        fontSize: 16,
+        fontWeight: '600',
+      },
+    })
   }
 });
 
